3561c9cbcbd846079a1962064e277d290007e4b9
Nico Skorzewski <n.skor1@gmail.com>
2019-01-21 20:12:52 -0800

    The Heart

---
+++ The Heart/Assets/Scripts/Heart_Touch_Anim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart_Touch_Anim : MonoBehaviour {
+
+    public GameObject cube_collide;
+    
+    private Animation anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animation>();
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "cube_collide")
+        {
+            
+            anim.Play("Icosphere|IcosphereAction");
+            Debug.Log("collision");
+        }
+    }
+}
---
+++ The Heart/Assets/Scripts/RotateAroundPoint.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAroundPoint : MonoBehaviour
+    
+{
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.down, 35 * Time.deltaTime);
+    }
+}
